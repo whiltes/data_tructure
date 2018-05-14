89c53016a3b0c59444e360e5ff36d86f7964143d
whiltes <541652148@qq.com>
2018-05-15 07:34:42 +0800

    LeetCode

---
+++ 1004母牛问题/1004母牛问题/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int a[54] = {1,2,3,4};
+//	int n = 0;
+//	int i = 0;
+//
+//	scanf("%d", &n);
+//	for(i=4; i<=54; i++)
+//	{
+//		a[i] = a[i-1] + a[i-3];
+//	}
+//	while(n!=0)
+//	{
+//		printf("%d\n", a[n-1]);
+//		scanf("%d", &n);
+//	}
+//
+//	return 0;
+//}
